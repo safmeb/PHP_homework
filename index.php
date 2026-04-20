bad090a4b981c280c22f94609329908342ba5cc8
Artem_Safon <coruscardea@gmail.com>
2026-04-20 20:49:22 +0300

    index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ do {
   $num2 = trim(fgets(STDIN)); // получаем переменную string
   $num2Int = intval($num2); // приводим к целочисленному типу
   $num2Float = floatval($num2);// приводим к типу с плавающей точкой
-  if (is_numeric($num2) && ($num2Int - $num2Float == 0) & $num2!=0) { // проверяем введенное значение на цифры или буквы
+  if (is_numeric($num2) && ($num2Int - $num2Float == 0) && $num2!=0) { // проверяем введенное значение на цифры или буквы
    echo "знаменатель: ". $num2 . PHP_EOL; // все ОК выводит значение знаменателя
    break;}
    if ($num2 ==0) {
